6a0dc6a7772f804ac56bf7b069b10e406f1fb449
Juan Bermudez <juanbermudez_10@ieee.org>
2024-02-01 14:16:46 -0500

    test

---
+++ part1-curso/src/main/java/com/juanbermudez/part1/curso/Part1Curso.java
@@ -0,0 +1,15 @@
+
+package com.juanbermudez.part1.curso;
+
+/**
+ *
+ * @author juanb
+ */
+public class Part1Curso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Salut monde!");
+        System.out.println("¡Hola mundo!");
+    }
+}
